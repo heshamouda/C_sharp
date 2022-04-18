dfce1fdf2ad422c2db06932e0b13eec4b9ac963d
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-04-18 22:04:41 +0200

    second part

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,9 +8,16 @@ namespace FirstConsoleApp
 {
     class Program
     {
+        public static string title = "c# App";
+        public static int whichPart;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C# Essential Training Part 1!");
+            whichPart = 2;
+            var message = string.Format("welcome to {0} Part {1}", title, whichPart);
+            Console.WriteLine("Welcome to C# Part 1!");
+            Console.WriteLine("Press the enter key to continue.");
+            Console.ReadLine();
+            Console.WriteLine(message);
             Console.WriteLine("Press the enter key to exit.");
             Console.ReadLine();
         }
