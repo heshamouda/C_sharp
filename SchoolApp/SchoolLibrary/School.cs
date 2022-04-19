89fa35b0be84df898308864d8eebe582795eb683
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-04-19 10:33:49 +0200

    - add methods to calculate the school's score -

--- SchoolApp/SchoolLibrary/School.cs
+++ SchoolApp/SchoolLibrary/School.cs
@@ -8,15 +8,15 @@ namespace SchoolLibrary
 {
     internal class School
     {
-       public string Name { get; set; }
-       public string Description { get; set; }
-       public string type { get; set; }
-       public string Address { get; set; }
-       public string SchoolCity { get; set; }
-       public string SchoolCountry { get; set; }
-       public string SchoolPhone { get; set; }
-       public string SchoolPostalCode { get; set; }
-       public string SchoolCountryCode { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string type { get; set; }
+        public string Address { get; set; }
+        public string SchoolCity { get; set; }
+        public string SchoolCountry { get; set; }
+        public string SchoolPhone { get; set; }
+        public string SchoolPostalCode { get; set; }
+        public string SchoolCountryCode { get; set; }
 
         private string _twitterAddress;
         public string TwitterAddress
@@ -25,16 +25,40 @@ namespace SchoolLibrary
             get { return _twitterAddress; }
             set
             {
-                if (value.StartsWith("@")) {
+                if (value.StartsWith("@"))
+                {
                     _twitterAddress = value.Substring(1);
                 }
                 else
                 {
                     throw new Exception("The twitter address must begin with @");
-                }    
+                }
             }
         }
 
+        public School()
+        {
+            // to handle initialisation of values
+            Name = "Untitled School";
+            SchoolPhone = "55 34 34 22";
+        }
 
+        public School(string SchoolName, string SchoolPhoneNumber)
+        {
+            Name = SchoolName;
+            SchoolPhone = SchoolPhoneNumber;
+        }
+
+        public float AverageThreeScores(float a, float b, float c)
+        {
+            var result = (a + b + c) / 3;
+            return result;
+        }
+
+        public int AverageThreeScores(int a, int b, int c)
+        {
+            var result = (a + b + c) / 3;
+            return result;
+        }
     }
 }
