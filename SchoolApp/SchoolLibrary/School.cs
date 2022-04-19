042e9b21c19fe81d6170bc83e2d7e7bf1125222b
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-04-19 09:40:32 +0200

    - Create New project School App - add class school

---
+++ SchoolApp/SchoolLibrary/School.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+    internal class School
+    {
+        string SchoolName;
+        string SchoolDescription;
+        string SchoolType;
+        string SchoolAdress;
+        string SchoolCity;
+        string SchoolCountry;
+        string SchoolPhone;
+        string SchoolPostalCode;
+        string SchoolCountryCode;
+        
+    }
+}
