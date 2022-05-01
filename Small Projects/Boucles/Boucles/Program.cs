c403881447a14cbedd1f087665ab3721c571e035
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-05-01 02:11:21 +0200

    add loop project

---
+++ Small Projects/Boucles/Boucles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Boucles 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=10; i++)
+            {
+                Console.WriteLine("Hey " + i);
+            }
+
+            int j = 1;
+            while(j < 100)
+            {
+                Console.WriteLine("Test " + j);
+                // ...
+                j++;
+            }
+
+            Console.WriteLine("Quel est le résultat de 3 + 4 ?");
+            string res = Console.ReadLine();
+            while (res != "7")
+            {
+                Console.WriteLine("Essaye encore !");
+                res = Console.ReadLine();
+            }
+            Console.WriteLine("Bravo !");
+        }
+    }
+}
